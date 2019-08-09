a2539eb31c154be7ffb978371fd8175f1f2f782b
Oleg Yavorsky <mw17@ukr.net>
2019-08-09 19:24:00 +0300

    modal added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Backdrop from "./components/Backdrop/Backdrop";
 import Button from './components/Button/Button';
+import Modal from "./components/Modal/Modal";
 
 class App extends Component {
   
@@ -10,7 +11,8 @@ class App extends Component {
     // isAuth: true,
     isAuth: false,
     authMode: "login",
-    error: null
+    //error : null 
+    error: 'Test for error'
   };
 
   constructor() {
@@ -20,14 +22,19 @@ class App extends Component {
     // this.client.callFunction('Greet', ['Max']);
   }
 
-render() {
-  
+  errorHandler = message => {
+    this.setState({
+      error: message
+    });
+  };
+
+render() {  
   return (
     <div className="App">
       <header className="App-header">
 
-<Backdrop show={!!this.state.error} />
-<Button type="button">Okay</Button>
+        <Backdrop show={!!this.state.error} />
+        <Button type="button">Okay</Button>
 
         <img src={logo} className="App-logo" alt="logo" />
         <a className="App-link" href = "E:\GitHub\LandingPage">E:\GitHub\LandingPage\</a>
@@ -36,6 +43,13 @@ render() {
         <a className="App-link" href="E:\MongoUdemyMax/18\stitch-01-starting-code">E:\MongoUdemyMax/18\stitch-01-starting-code</a>
         
       </header>
+      <Modal
+          open={!!this.state.error}
+          title="An Error Occurred"
+          onClose={() => this.errorHandler(null)}
+        >
+          <p>{this.state.error}</p>
+        </Modal>
 
     </div>
   );
