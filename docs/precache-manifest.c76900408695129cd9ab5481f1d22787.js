bc3aa697109ec2640608d8ec9f76deb64717ff49
Oleg Yavorsky <mw17@ukr.net>
2019-08-09 19:54:54 +0300

    Rename  build  docs
     "build": "react-scripts build && Rename  build  docs",

---
+++ docs/precache-manifest.c76900408695129cd9ab5481f1d22787.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f64ded7bacc16d088781929e30d3ca57",
+    "url": "/LandingPage/index.html"
+  },
+  {
+    "revision": "66c0f752915faf95fe9d",
+    "url": "/LandingPage/static/css/main.0b3f0378.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/LandingPage/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "66c0f752915faf95fe9d",
+    "url": "/LandingPage/static/js/main.4543106e.chunk.js"
+  },
+  {
+    "revision": "9ff9668da66d8416cbc2",
+    "url": "/LandingPage/static/js/runtime~main.6de79c0c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/LandingPage/static/media/logo.5d5d9eef.svg"
+  }
+]);
